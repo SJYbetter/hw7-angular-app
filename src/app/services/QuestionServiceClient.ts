91034c0946f92d00a83684bbabfbbdae40d6e09f
SJYbetter <sjyhyn@gmail.com>
2020-04-08 14:27:57 -0400

    finish

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://pure-mesa-39023.herokuapp.com/api/quizzes/${qid}/questions`)
+    fetch(`https://pure-mesa-39023.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -2,10 +2,10 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://pure-mesa-39023.herokuapp.com/api/quizzes')
+    fetch('https://pure-mesa-39023.herokuapp.com/api/quizzes')
       .then(response => response.json())
-  
+
   findQuizById = (qid) =>
-    fetch(`http://pure-mesa-39023.herokuapp.com/api/quizzes/${qid}`)
+    fetch(`https://pure-mesa-39023.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
