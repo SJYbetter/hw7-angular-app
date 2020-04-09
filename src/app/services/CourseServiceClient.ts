561e168331566a30befa125c76cb6d8c5d732a40
SJYbetter <sjyhyn@gmail.com>
2020-04-09 16:19:10 -0400

    change https

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,18 +3,18 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findCourseById = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001821034/${courseId}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001821034/${courseId}`)
       .then(response => response.json());
 
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/001821034/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/001821034/courses')
       .then(response => response.json());
 
   findModulesForCourse = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001821034/courses/${courseId}/modules`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001821034/courses/${courseId}/modules`)
       .then(response => response.json());
 
   findLessonsForModule = (moduleId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001821034/modules/${moduleId}/lessons`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001821034/modules/${moduleId}/lessons`)
       .then(response => response.json());
 }
